ab37bd0e61b692b9f71bf8f15a2f9fb6b78fb3ee
hoclamwe <31909579+hoclamwe@users.noreply.github.com>
2017-11-06 15:19:44 +0700

    Create nojs.js

---
+++ nojs.js
@@ -0,0 +1,10 @@
+function checkRef() {
+	var refe = document.referrer;
+     var str = top.location.href;
+     var ref = str.split('http://')[1].split('/')[0];
+         if(ref == 'xembong365.blogspot.com' || ref == 'www.xemtiviso.com' || ref == 'sv2.xemtiviso.com' ||  ref == 'www.tivi365.net' || ref == 'www.xemtivitt.net' || ref == 'sv2.xemtivitt.net' || ref == 'sv.xemtivitt.net' || ref == 'www.kenhtivihd.net' || ref == 'xd.kenhtivihd.net' || ref == 'www.tvvn.net' || ref == 'xd.tvvn.net' || ref == 'www.xembongda24h.net' || ref == 'live.xembongda24h.net' || ref == 'www.xembongdanet.com' || ref == 'xembong365.com' || ref == 'www.xemtivi360.com' || ref == 'xd.xemtivi360.com' || ref == 'www.xemtivifull.com' || ref == 'xd.xemtivifull.com' || ref == 'www.tv4d.net' || ref == 'xd.tv4d.net' || ref == 'www.tv8g.com' || ref == 'xd.tv8g.com' || ref == 'xd.tivi4k.net' || ref == 'www.tivi4k.net' && refe !='')  {}
+		else {
+			document.write('<meta http-equiv="refresh" content="0;http://www.tivi4k.net">');
+		}
+}
+checkRef();
